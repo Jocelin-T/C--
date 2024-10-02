21724f04cc8eb5092bb7adcdac22bf065666abd8
Jocelin Thumelin <jocelin.thumelin@formation.orif.ch>
2024-10-02 15:51:21 +0200

    std::array

--- Advanced/Other/API_Design.cpp
+++ Advanced/Other/API_Design.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-// #################################### API Design ####################################
-// Notes:   Here some rules for API Design:
-//          - pimpl => C++/Advanced/Pointers/Implementation_Pointers.cpp (Hide private members)
-//          - passing Struct of options as parameters to functions, to avoid lot of arguments. 
-
+/* #################################### API Design ####################################
+* Notes:   Here some rules for API Design:
+*          - pimpl => C++/Advanced/Pointers/Implementation_Pointers.cpp (Hide private members)
+*          - passing Struct of options as parameters to functions, to avoid lot of arguments. 
+*/
 
 int main(){
 
--- Advanced/STL/STL_Base/Array.cpp
+++ Advanced/STL/STL_Base/Array.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
+#include <array>
 
-// #################################### std::array ####################################
-// Notes:   
+/* #################################### std::array ####################################
+* Notes:    Fixed size
+*           Allocated on the Stack
+            Safer than [] 
+*/
 
 
-void printArray(int array[4]){
-    std::cout << sizeof(array) << '\n';
+// C-style method to print the size of an array size
+void printArray(int array[4], int size){
+    std::cout << sizeof(int) * size << '\n';
+}
+
+// STL array
+void printArraySTL(std::array<int, 4> param){
+    std::cout << sizeof(param) << '\n'; // more safe compare to C-style
+    std::cout << param.size() << '\n'; // return the number of elements
+    std::cout << param.at(2) << '\n'; // return the value (like [2])
+}
+
+// Still possible to access underline element, like pointer
+void lecacyCCodeBase(int* array, int size){
+
 }
 
 int main(){
 
     // static, C-style arrays
-    int data[4];
+    int arr[4];
     int init[] {1, 2, 3};
 
-    std::cout << sizeof(data) << '\n';
+    std::cout << sizeof(arr) << '\n';
+    printArray(arr, 4);
 
-    printArray(data);
+    // STL style array
+    std::array<int, 4> arr_stl {1,2,3,4};
+    printArraySTL(arr_stl);
+
+    // .data() is like passing the pointer of the array
+    lecacyCCodeBase(arr_stl.data(), arr_stl.size());
+
 
     return 0;
 }
